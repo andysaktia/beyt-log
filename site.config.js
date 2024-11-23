9ce015551bb780ddb3c1a2618f36f0f1e09258c3
andysaktia <37647600+andysaktia@users.noreply.github.com>
2024-11-23 12:03:33 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,7 +2,7 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "beytmin",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/beyt-black.png", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "writer",
     bio: "I write everything about my hobby.",
     email: "andysaktia@gmail.com",
